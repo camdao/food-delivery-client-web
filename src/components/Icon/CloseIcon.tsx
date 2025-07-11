e356c258f5d67c2a05bfda26a5a291fcb6cda7f5
camdao <luongcamdao@gmail.com>
2025-07-11 22:06:22 +0700

    add onClick Icon close

--- src/components/Icon/CloseIcon.tsx
+++ src/components/Icon/CloseIcon.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-const CloseIcon: React.FC<React.SVGProps<SVGSVGElement>> = () => {
+import { type IconComponentProps } from '.';
+
+function CloseIcon(props: IconComponentProps) {
   return (
-    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg {...props} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
     <circle cx="10" cy="10" r="10" fill="#CDCDCF"/>
     <path d="M12.9657 7L6.63232 13" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     <path d="M6.63232 7L12.9657 13" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
