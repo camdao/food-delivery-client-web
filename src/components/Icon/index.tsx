e5349969a1d7e012fd572817f4fb671e38ef46b9
camdao <luongcamdao@gmail.com>
2025-07-21 15:02:55 +0700

    add component password

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -2,13 +2,14 @@ import { type SVGProps } from "react";
 
 import CardIcon from "./CardIcon";
 import CloseIcon from "./CloseIcon";
+import HiddenIcon from "./HiddenIcon";
 import MenuIcon from "./MenuIcon";
 
 export const IconComponentMap = {
     'menu': MenuIcon,
     'card': CardIcon,
-    'close': CloseIcon
-
+    'close': CloseIcon,
+    'hidden': HiddenIcon
 }
 interface Props extends IconComponentProps{
     name: keyof typeof IconComponentMap;
